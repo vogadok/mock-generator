0e14d78b957acf04af3c944866cb2eb7725d16f3
Kevin Vogado <kevin.vogado@topi.io>
2022-12-26 15:11:47 -0300

    ok dnv

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const router = express.Router();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+'/public/views/test2.html'));
+    res.sendFile(path.join(__dirname+'/public/views/home.html'));
 })
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is Running"))
