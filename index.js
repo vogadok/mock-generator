65d239fb58df6f5a58d61a8bec872a7cc2f3032c
Kevin Vogado <kevin.vogado@topi.io>
2022-12-26 16:46:35 -0300

    ok

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const express = require('express')
 const app = express();
 const path = require('path');
-const home = require('./public/views/home.html')
-
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use("/public/views", home);
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname+'/public/views/home.html'));
